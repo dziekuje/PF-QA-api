7b1935aa243d0caf9b61448ab9a6ae4a650e5f59
Andrew Mac <hub.mnk+git@gmail.com>
2020-10-10 16:49:37 +0200

    add options for cli execution

---
+++ practice/3_pytest_addoption/addoption.py
@@ -0,0 +1,7 @@
+def test_answer(url_param):
+    if url_param == "interia.pl":
+        print("Interia!")
+    elif url_param == "wp.pl":
+        print("WP!")
+    else:
+        print("Something different!")
