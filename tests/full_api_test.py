bc16120ba04e865a2e005350eca6817a9b49cbe5
Andrew Mac <hub.mnk+git@gmail.com>
2020-10-14 22:39:41 +0200

    Parametrized API requests with API Client class

--- tests/full_api_test.py
+++ tests/full_api_test.py
@@ -10,22 +10,30 @@ def test_api_response_is_200(api_client):
 
 def test_api_get_all_users(api_client):
     res = api_client.get(path="/posts")
-    # print(res.json())
     assert res.status_code == 200
     assert len(res.json()) > 1
+    print(res.json())
+    print(type(res.json()))
+    print(len(res.json()))
 
 
 @pytest.mark.parametrize("id", [1, 2, 3, 4])
 def test_api_get_user_by_ID(api_client, id):
     res = api_client.get(path=f"/posts/{id}")
     print(res.json())
+    print(type(res.json()))
     assert res.status_code == 200
     assert res.json()["id"] == id
 
 
 @pytest.mark.parametrize("user_id", [1, 2, 3, 4])
-def test_api_get_user_by_userID(api_client, user_id):
+def test_api_sort_user_by_userID(api_client, user_id):
     res = api_client.get(path="/posts", params=f"userId={user_id}")
-    print(res.json())
     assert res.status_code == 200
     assert res.json()[0]["userId"] == user_id
+    print(type(res.json()))
+    print(res.json())
+    print(type(res.json()[0]))
+    print(res.json()[0])
+    print(type(res.json()[1]))
+    print(res.json()[1])
